a48c6f46ffc6c3e7c756d5e3e573f47e8356e80d
aratoca <afbayonac@gmail.com>
2022-12-17 20:37:43 -0500

    Add hero

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+import "./styles/reset.css"
+import "./styles/globals.css"
+import "./styles/hero.css"
+
+
 const log = (...args) => console.log('[priority-treemap]', ...args)
 
 log('init [🦥]')
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,44 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+const config = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
   ]
+}
+
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.watch = true
+
+    config.devServer = {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+    }
+  }
+
+  return config
 }
