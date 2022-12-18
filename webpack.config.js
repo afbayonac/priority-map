1825f5671b3ef335259928e94e4dac4d97b3d157
aratoca <afbayonac@gmail.com>
2022-12-18 10:39:43 -0500

    Config base

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,49 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin")
 
-const config = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "public"},
-      ]
-    })
-  ]
-}
-
-
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.watch = true
 
-    config.devServer = {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      compress: true,
-      port: 9000,
+  return {
+    ...( argv.mode === 'development'
+    ? {
+      watchpack: true,
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+      }
     }
+    : {}
+    ),
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        ...(argv.mode === 'production' ? { base: 'https://afbayonac.github.io/priority-treemap/' } : {})
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "public" },
+        ]
+      })
+    ]
   }
-
-  return config
 }
